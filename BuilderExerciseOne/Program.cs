0277e1ac800eb44e8097a901e0314974daa103f9
Bryan Pritchett <bryan@coderbox.co>
2018-03-07 22:19:23 -0500

    update builder exercise one, not working yet

--- BuilderExerciseOne/Program.cs
+++ BuilderExerciseOne/Program.cs
@@ -5,29 +5,25 @@ namespace BuilderExerciseOne
 {
     public class CodeBuilder
     {
-        StringBuilder codeString = new StringBuilder();
+        private StringBuilder codeString = new StringBuilder();
         
         public CodeBuilder(string className){
-            codeString.Append($"public class {className}\\n{{\\n");
+            codeString.Append($"public class {className}\n{{\n");
             
         }
         
-        public StringBuilder AddField(string fieldName, string stringType){
+        public CodeBuilder AddField(string fieldName, string stringType){
             codeString.Append($"  public {stringType} {fieldName};\n");
-            return codeString;
+            return this;
 
         }
 
-//        public override string WriteLine()
-//        {
-//            return codeString.Append($"}}").ToString();
-//        }
     }
     
     public class Program
     {
         static void Main(){
-            var cb = new CodeBuilder("Person").AddField("Name","string");//.AddField("Age","int");
+            CodeBuilder cb = new CodeBuilder("Person").AddField("Name","string").AddField("Age","int");
             Console.WriteLine(cb);
         }
     }
