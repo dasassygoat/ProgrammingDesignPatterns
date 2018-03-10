76e3114b23291b92165a73c6a13254a031633a2e
Bryan Pritchett <bryan@coderbox.co>
2018-03-09 23:39:15 -0500

    factory as part of the class that is being built

--- Factories/Program.cs
+++ Factories/Program.cs
@@ -3,18 +3,7 @@ using static System.Console;
 
 namespace Factories
 {
-    public static class PointFactory
-    {
-        public static Point NewPolorPoint(double rho, double theta)
-        {
-            return new Point(rho*Math.Cos(theta), rho*Math.Sin(theta));
-        }
-
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-    }
+    
     public class Point
     {
         
@@ -31,12 +20,26 @@ namespace Factories
         {
             return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
         }
+        
+        public static class Factory
+        {
+            public static Point NewPolorPoint(double rho, double theta)
+            {
+                return new Point(rho*Math.Cos(theta), rho*Math.Sin(theta));
+            }
+
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+        }
+        
     }
     class Program
     {
         static void Main(string[] args)
         { 
-            var point = PointFactory.NewPolorPoint(1.0, Math.PI / 2);
+            var point = Point.Factory .NewPolorPoint(1.0, Math.PI / 2);
             WriteLine(point);
         }
     }
