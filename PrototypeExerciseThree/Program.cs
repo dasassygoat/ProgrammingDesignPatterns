d99fabc6444178a55f9e38eee342ce3108884aa7
Bryan Pritchett <bryan@coderbox.co>
2018-03-20 22:59:14 -0400

    Need implementation

--- PrototypeExerciseThree/Program.cs
+++ PrototypeExerciseThree/Program.cs
@@ -21,7 +21,20 @@ namespace PrototypeExerciseThree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var line = new Line();
+            line.Start = new Point();
+            line.Start.X = 199;
+            line.Start.Y = 234;
+            line.End = new Point();
+            line.End.X = 233;
+            line.End.Y = 199;
+            
+            var line2 = line.DeepCopy();
+            line2.Start.X = 45;
+            
+            System.Console.WriteLine($"Line 1 Start X: {line.Start.X}");
+            System.Console.WriteLine($"Line 2 Start X: {line2.Start.X}");
+
         }
     }
 }
